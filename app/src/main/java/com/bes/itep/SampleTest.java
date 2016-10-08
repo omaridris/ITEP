eb0fe34938ca4608e793148a4b55d0e3b6bf1cd1
omaridris <Omar@Omars-MacBook-Pro.local>
2016-10-08 10:55:04 -0700

    First commit

---
+++ app/src/main/java/com/bes/itep/SampleTest.java
@@ -0,0 +1,13 @@
+package com.bes.itep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_test);
+    }
+}
